e12bbfc883d6097e9d45d814b6466fdee7874827
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-03 19:09:54 +1000

    Create speechSynthesis

--- client/my-app/src/components/App.js
+++ client/my-app/src/components/App.js
@@ -1,5 +1,28 @@
 import React, { Component } from "react";
-import Articles from './Articles';
+import Articles from "./Articles";
+
+const synth = window.speechSynthesis;
+
+const speak = text => {
+  if (synth.speaking) {
+    console.error("speechSynthesis.speaking");
+    return;
+  }
+  if (text !== "") {
+    var utterThis = new SpeechSynthesisUtterance(text);
+    utterThis.onend = function(event) {
+      console.log("SpeechSynthesisUtterance.onend");
+    };
+    utterThis.onerror = function(event) {
+      console.error("SpeechSynthesisUtterance.onerror");
+    };
+
+    utterThis.voice = synth.getVoices()[0];
+    utterThis.pitch = "1";
+    utterThis.rate = "1";
+    synth.speak(utterThis);
+  }
+};
 
 class App extends Component {
   constructor() {
@@ -16,12 +39,12 @@ class App extends Component {
       .then(response => response.json())
       .then(articles => self.setState({ articles }))
       .catch(error => console.log("parsing failed", error));
+
+    speak("Content loaded!");
   }
 
   render() {
-    return (
-      <Articles this={this}/>
-    );
+    return <Articles this={this} />;
   }
 }
 

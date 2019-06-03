a83657d15ae449e20b2653d00e80ce1dc588ddac
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-03 20:21:24 +1000

    Add icons

--- client/my-app/src/components/FeedItem.js
+++ client/my-app/src/components/FeedItem.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHeadphones, faShare } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+library.add(
+  faHeadphones,
+  faShare
+)
 
 const FeedItem = props => (
   <li key={props.item.title}>
     <h3>{props.item.title}</h3>
     <p>{props.item.summary}</p>
+    <FontAwesomeIcon icon={faHeadphones} />
+    <FontAwesomeIcon icon={faShare} />
   </li>
 )
 

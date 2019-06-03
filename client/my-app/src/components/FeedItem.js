2a2f184b3c60535bbdf07836bbeb549be0266c60
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-03 21:36:38 +1000

    Speak title and summary when clicked on headphone

--- client/my-app/src/components/FeedItem.js
+++ client/my-app/src/components/FeedItem.js
@@ -2,6 +2,7 @@ import React from "react";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faHeadphones, faShare } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import speak from "./speak";
 
 
 library.add(
@@ -9,11 +10,18 @@ library.add(
   faShare
 )
 
+
+function handleClick(item) {
+  speak(item.title)
+  speak(item.summary)
+}
+
+
 const FeedItem = props => (
   <li key={props.item.title}>
     <h3>{props.item.title}</h3>
     <p>{props.item.summary}</p>
-    <FontAwesomeIcon icon={faHeadphones} />
+    <FontAwesomeIcon onClick={() => handleClick(props.item)} icon={faHeadphones} />
     <FontAwesomeIcon icon={faShare} />
   </li>
 )

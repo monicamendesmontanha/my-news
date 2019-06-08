34be01c8a129e0e83180f46df6bd5eb5e4c3c318
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-08 15:46:45 +1000

    Use crawler to fetch through selector to show text

--- server/crawler.js
+++ server/crawler.js
@@ -1,42 +1,37 @@
-////////////////////////////////
-// CRAWLER ////////////////////
 const Crawler = require('crawler');
 
-app.get("/article/:url", (req, res) => {
-  const url = req.params.url;
-
-  // chama o código do crawler aqui passando a url como parâmetro
-
-  const crawler = new Crawler({
-    maxConnections: 10,
-    callback: (error, res, done) => {
-      if (error) {
-        console.log(error);
-      }
-      done();
-    },
-  });
-
-  crawler.queue([
-    {
-      url:
-        'https://www.nytimes.com/2019/06/04/world/europe/brexit-trump-theresa-may.html',
-      callback: (error, crawlerResponse, done) => {
-        let result; // result can be error or success
-
-        if (error) {
-          console.log(error);
-        } else {
-          const { $ } = crawlerResponse;
-          result = $('section[name="articleBody"]').text()
-          //console.log('>>>>>>> NYTIMES')
-          //console.log($('section[name="articleBody"]').text());
-        }
-        // return JSON with content
-        res.json({content: $('section[name="articleBody"]').text()),
-        done();
-      },
+const crawler = new Crawler({
+  maxConnections: 10,
+  callback: (error, res, done) => {
+    if (error) {
+      console.error(error);
     }
-  ]);
 
+    done();
+  },
 });
+
+const fetchContent = async (url) => {
+	let result;
+
+	crawler.queue([
+		{
+			url:
+				'https://www.nytimes.com/2019/06/04/world/europe/brexit-trump-theresa-may.html',
+			callback: (error, crawlerResponse, done) => {
+				if (error) {
+					console.error(error);
+				} else {
+					const { $ } = crawlerResponse;
+					result = $('section[name="articleBody"]').text()
+				}
+
+				done();
+			},
+		}
+	]);
+
+	return result;
+};
+
+module.exports = { fetchContent };
--- server/index.js
+++ server/index.js
@@ -3,31 +3,42 @@ const app = express();
 const port = 8000;
 const feedparser = require("feedparser-promised");
 const cors = require("cors");
+const Crawler = require('crawler');
+
+const crawler = new Crawler({
+  maxConnections: 10,
+  callback: (error, res, done) => {
+    if (error) {
+      console.error(error);
+    }
+
+    done();
+  },
+});
 
 const MAX_FEEDS = 8;
 
 app.use(cors());
 
-app.get("/feed", (req, res) => {
-  const feeds = [
-    {
-      id: "The Sydney Morning Herald",
-      url: "https://www.smh.com.au/rss/feed.xml"
-    },
-    {
-      id: "The Economist",
-      url: "https://www.economist.com/finance-and-economics/rss.xml"
-    },
-    {
-      id: "BBC News - Technology",
-      url: "http://feeds.bbci.co.uk/news/technology/rss.xml"
-    },
-    {
-      id: "New York Times",
-      url: "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"
-    }
-  ];
+const feeds = [
+  {
+    id: "The Economist",
+    selector: '.blog-post__text',
+    url: "https://www.economist.com/finance-and-economics/rss.xml"
+  },
+  {
+    id: "BBC News - Technology",
+    selector: '[class="story-body__inner"]',
+    url: "http://feeds.bbci.co.uk/news/technology/rss.xml"
+  },
+  {
+    id: "New York Times",
+    selector: 'section[name="articleBody"]',
+    url: "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"
+  }
+];
 
+app.get("/feed", (req, res) => {
   const promises = feeds.map(feed => feedparser.parse(feed.url));
 
   Promise.all(promises).then(result => {
@@ -45,9 +56,32 @@ app.get("/feed", (req, res) => {
 });
 
 app.get("/feed/article", (req, res) => {
-  const url = req.query.url
+  const url = unescape(req.query.url);
+  const feedId = unescape(req.query.feedId);
 
-  res.json({ url: url, content: "bla" });
+  crawler.queue([
+		{
+			url: url,
+			callback: (error, crawlerResponse, done) => {
+        let content;
+
+				if (error) {
+					console.error(error);
+				} else {
+          const { $ } = crawlerResponse;
+
+          const feedFound = feeds.find(feed => feed.id === feedId);
+
+          if (feedFound) {
+            content = $(feedFound.selector).text();
+          }
+				}
+
+        res.json({ url: url, content: content });
+        done();
+			},
+		}
+  ]);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));

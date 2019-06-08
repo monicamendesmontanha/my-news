f1eb81825f7e70f13f558494c6c2b82029066467
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-08 22:16:20 +1000

    Destruct share component

--- client/my-app/src/components/Share.js
+++ client/my-app/src/components/Share.js
@@ -15,24 +15,24 @@ import {
 } from "react-share";
 import "./Share.scss";
 
-const Share = props => (
-    <div className="share">
-      <LinkedinShareButton url={props.item.link}>
-        <LinkedinIcon className="shareIcon" size={28} iconBgStyle={{fill:"white"}} logoFillColor={'#333333'}/>
-      </LinkedinShareButton>
-      <TwitterShareButton url={props.item.link}>
-        <TwitterIcon className="shareIcon" size={28} iconBgStyle={{fill:"white"}} logoFillColor={'#333333'}/>
-      </TwitterShareButton>
-      <WhatsappShareButton url={props.item.link}>
-    <WhatsappIcon className="shareIcon" size={28} iconBgStyle={{fill:"white"}} logoFillColor={'#333333'} />
-      </WhatsappShareButton>
-      <FacebookShareButton url={props.item.link}>
-        <FacebookIcon className="shareIcon" size={28} iconBgStyle={{fill:"white"}} logoFillColor={'#333333'}/>
-      </FacebookShareButton>
-      <EmailShareButton url={props.item.link}>
-        <EmailIcon className="shareIcon" size={28} iconBgStyle={{fill:"white"}} logoFillColor={'#333333'}/>
-      </EmailShareButton>
-    </div>
+const Share = ({ item, size = 27 }) => (
+  <>
+    <LinkedinShareButton url={item.link}>
+      <LinkedinIcon size={size} iconBgStyle={{fill:"white"}} logoFillColor={'#333333'}/>
+    </LinkedinShareButton>
+    <TwitterShareButton url={item.link}>
+      <TwitterIcon size={size} iconBgStyle={{fill:"white"}} logoFillColor={'#333333'}/>
+    </TwitterShareButton>
+    <WhatsappShareButton url={item.link}>
+  <WhatsappIcon size={size} iconBgStyle={{fill:"white"}} logoFillColor={'#333333'} />
+    </WhatsappShareButton>
+    <FacebookShareButton url={item.link}>
+      <FacebookIcon size={size} iconBgStyle={{fill:"white"}} logoFillColor={'#333333'}/>
+    </FacebookShareButton>
+    <EmailShareButton url={item.link}>
+      <EmailIcon size={size} iconBgStyle={{fill:"white"}} logoFillColor={'#333333'}/>
+    </EmailShareButton>
+  </>
 );
 
 export default Share;

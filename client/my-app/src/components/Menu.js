a03c1d44410f2a8da67708741b2b13f313a2560e
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-11 09:45:56 +1000

    Cleanup code

--- client/my-app/src/components/Menu.js
+++ client/my-app/src/components/Menu.js
@@ -1,24 +1,7 @@
 import React from "react";
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faGlobe, faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-library.add(
-  faGlobe,
-  faSun,
-  faMoon
-)
 
 const Menu = props => (
-  <>
     <h1>My News</h1>
-    {/* TO DO MODE DARK/LIGHT: */}
-    {/* <FontAwesomeIcon icon={faSun} /> */}
-    {/* toogle */}
-    {/* <FontAwesomeIcon icon={faMoon} /> */}
-    {/* TO DO SELECT LANGUAGE */}
-    {/* <FontAwesomeIcon onClick={() => selectLanguage(props.item)} icon={faGlobe} className="dropDown" /> */}
-  </>
 )
 
 export default Menu;

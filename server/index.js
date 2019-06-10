e4c0a8cec4d8e270f48852a6e2ecc0945b06b91d
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-10 11:04:58 +1000

    Add dynamic port based on the env var

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 const feedparser = require("feedparser-promised");
 const cors = require("cors");
 const Crawler = require('crawler');
